83f7244380e188e6e939857c29bc2fb6b8f0f0aa
lasalesi <larsericmueller@gmail.com>
2022-02-07 14:56:17 +0100

    Create filters.py
    add first filter set

---
+++ itst/itst/filters.py
@@ -0,0 +1,14 @@
+# Copyright (c) 2022, libracore and Contributors
+# License: GNU General Public License v3. See license.txt
+
+from __future__ import unicode_literals
+import frappe
+
+# searches for activity item (category)
+def supplier_by_capability(doctype, txt, searchfield, start, page_len, filters):
+    return frappe.db.sql(
+        """SELECT `tabItem`.`name`, `tabItem`.`item_group`
+           FROM `tabItem`
+           WHERE `tabItem`.`item_group` LIKE "%{c}%" AND `tabItem`.`name` LIKE "%{s}%";
+        """.format(c=filters['keyword'], s=txt))
+  
