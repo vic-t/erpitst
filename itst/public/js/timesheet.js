b405e0905dfc1f2fa11ce18d022349f00ac9002c
lasalesi <larsericmueller@gmail.com>
2022-05-05 23:35:21 +0200

    timesheet: record duration in minutes

--- itst/public/js/timesheet.js
+++ itst/public/js/timesheet.js
@@ -5,3 +5,15 @@ frappe.ui.form.on('Timesheet', {
         });
     }
 });
+
+frappe.ui.form.on('Timesheet Detail', {
+    duration(frm, cdt, cdn) {
+        var duration_str = frappe.model.get_value(cdt, cdn, 'duration');
+        if (duration_str) {
+            var str_parts = duration_str.split(":")
+            var duration_min = parseInt(str_parts[0]) * 60 + parseInt(str_parts[1]);
+            var duration_h = duration_min/60;
+            frappe.model.set_value(cdt, cdn, 'hours', duration_h);
+        }
+    }
+});
