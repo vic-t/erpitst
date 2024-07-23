a9e00e0e4d99ca0db2a820e4fe2b784090ad8452
Lars <lars.mueller@libracore.com>
2024-07-23 19:05:07 +0000

    Modified cut-off condition (so it does not need the show invoiced flag)

--- itst/itst/report/offene_ts_positionen/offene_ts_positionen.py
+++ itst/itst/report/offene_ts_positionen/offene_ts_positionen.py
@@ -65,8 +65,10 @@ def get_data(filters):
     if cint(filters.show_all) == 0:
         customer_condition += """ AND  `tabProject`.`status` = "Open" """
     
-    # if show_invoiced positions: = 1 no filter, otherwise only uninvoiced
-    if cint(filters.show_invoiced) == 0:
+    # if show_invoiced positions: = 1 no filter, otherwise only uninvoiced (or cut-off date)
+    if filters.to_date:
+        customer_condition += """ AND (`tabSales Invoice`.`posting_date` IS NULL OR `tabSales Invoice`.`posting_date` >= "{to_date}") """.format(to_date=filers.to_date)
+    elif cint(filters.show_invoiced) == 0:
         customer_condition += """ AND `tabSales Invoice Item`.`name` IS NULL """
     
     sql_query = """
@@ -79,7 +81,7 @@ def get_data(filters):
             SUM(`tabTimesheet Detail`.`costing_amount`) AS `costing_amount`, 
             MIN(`tabTimesheet Detail`.`from_time`) AS `oldest`,
             MAX(`tabTimesheet Detail`.`to_time`) AS `newest`,
-            `tabCustomer`.`customer_name` AS `customer_name`,
+            (SELECT `customer_name` FROM `tabCustomer` WHERE `tabCustomer`.`name` = `tabProject`.`customer`) AS `customer_name`,
             `tabProject`.`status` AS `status`,
             0 AS `indent`
         FROM `tabTimesheet Detail`
@@ -90,14 +92,12 @@ def get_data(filters):
         )
         LEFT JOIN `tabSales Invoice` ON `tabSales Invoice`.`name` = `tabSales Invoice Item`.`parent`
         LEFT JOIN `tabProject` ON `tabProject`.name = `tabTimesheet Detail`.`project`
-        LEFT JOIN `tabCustomer` ON `tabCustomer`.`name` = `tabProject`.`customer`
         WHERE 
            `tabTimesheet`.`docstatus` = 1
            AND `tabTimesheet Detail`.`project` IS NOT NULL
            AND `tabTimesheet Detail`.`project` LIKE "{project}"
            AND ((`tabTimesheet Detail`.`from_time` >= "{from_date}" AND `tabTimesheet Detail`.`from_time` <= "{to_date}")
             OR (`tabTimesheet Detail`.`to_time` >= "{from_date}" AND `tabTimesheet Detail`.`to_time` <= "{to_date}"))
-           AND (`tabSales Invoice`.`posting_date` IS NULL OR `tabSales Invoice`.`posting_date` >= "{to_date}")
            AND {customer_condition}
         GROUP BY 
             `tabTimesheet Detail`.`project`;
