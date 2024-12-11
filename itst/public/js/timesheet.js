dc894c77f4f33c7fba232fd917e98909ccc8414e
Elia Rütsche <er@itst.ch>
2024-12-11 14:57:47 +0100

    cleaned code for custom script timesheet.js

--- itst/public/js/timesheet.js
+++ itst/public/js/timesheet.js
@@ -1,32 +1,3 @@
-frappe.ui.form.on('Timesheet', {
-    refresh(frm) {
-        frm.add_custom_button(__("Offene TS-Positionen"), function() {
-            frappe.set_route("query-report", "Offene TS-Positionen", {"employee": frm.doc.employee});
-        });
-    }
-});
-
-frappe.ui.form.on('Timesheet Detail', {
-    duration(frm, cdt, cdn) {
-        var duration_h = get_hrs_from_duration(frappe.model.get_value(cdt, cdn, 'duration'));
-        frappe.model.set_value(cdt, cdn, 'hours', duration_h);
-    },
-    billing_duration(frm, cdt, cdn) {
-        var duration_h = get_hrs_from_duration(frappe.model.get_value(cdt, cdn, 'billing_duration'));
-        frappe.model.set_value(cdt, cdn, 'billing_hours', duration_h);
-    }
-});
-
-function get_hrs_from_duration(duration) {
-    var duration_h = null;
-    if (duration) {
-        var str_parts = duration.split(":");
-        var duration_min = parseInt(str_parts[0]) * 60 + parseInt(str_parts[1]);
-        duration_h = duration_min/60;
-    }
-    return duration_h;
-}
-
 // this mapping table defines which item is used for a activity type - kategorie combination
 var mapping_table = {
     'Support': {
@@ -47,6 +18,10 @@ var mapping_table = {
  
 frappe.ui.form.on('Timesheet', {
 	refresh(frm) {
+        // Timesheet Report Button
+        frm.add_custom_button(__("Offene TS-Positionen"), function() {
+            frappe.set_route("query-report", "Offene TS-Positionen", {"employee": frm.doc.employee});
+        });
 		// category filters
 		frm.fields_dict.time_logs.grid.get_field('category').get_query =
             function(doc, cdt, cdn) {
@@ -93,6 +68,14 @@ frappe.ui.form.on('Timesheet', {
 });
  
 frappe.ui.form.on('Timesheet Detail', {
+    duration(frm, cdt, cdn) {
+        var duration_h = get_hrs_from_duration(frappe.model.get_value(cdt, cdn, 'duration'));
+        frappe.model.set_value(cdt, cdn, 'hours', duration_h);
+    },
+    billing_duration(frm, cdt, cdn) {
+        var duration_h = get_hrs_from_duration(frappe.model.get_value(cdt, cdn, 'billing_duration'));
+        frappe.model.set_value(cdt, cdn, 'billing_hours', duration_h);
+    },
     time_logs_add(frm, cdt, cdn) {
         /* this is a hotfix to clear project and task in new rows */
         frappe.model.set_value(cdt, cdn, "project", null);
@@ -132,7 +115,17 @@ frappe.ui.form.on('Timesheet Detail', {
         frappe.model.set_value(cdt, cdn, 'activity_type', locals[cdt][cdn].aktivitaet);
     }
 });
- 
+
+function get_hrs_from_duration(duration) {
+    var duration_h = null;
+    if (duration) {
+        var str_parts = duration.split(":");
+        var duration_min = parseInt(str_parts[0]) * 60 + parseInt(str_parts[1]);
+        duration_h = duration_min/60;
+    }
+    return duration_h;
+}
+
 function show_zammad_link(frm, cdt, cdn) {
     var child = locals[cdt][cdn];
 	if (child.zammad_ticket) {
