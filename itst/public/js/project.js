e2054f05bb5624e41dab6475bb73bca3039141e9
Elia Rütsche <er@itst.ch>
2024-12-11 15:51:37 +0100

    migrated custom script project to existing code file

--- itst/public/js/project.js
+++ itst/public/js/project.js
@@ -1,7 +1,17 @@
 frappe.ui.form.on('Project', {
-    refresh(frm) {
+	refresh(frm) {
+	    // display notes at the top
+		if (frm.doc.notes) {
+		    cur_frm.dashboard.add_comment( frm.doc.notes , 'green', true);
+ 
+		    var elements = document.getElementsByClassName("green");
+		    for (var i = 0; i < elements.length; i++) {
+		        elements[i].style.setProperty('color', 'dimgray', 'important');
+		    }
+		}
+
         frm.add_custom_button(__("Offene TS-Positionen"), function() {
             frappe.set_route("query-report", "Offene TS-Positionen", {"project": frm.doc.name});
         });
-    }
+	}
 });
