649a42127a34234670f5f4f5676ca284490491c3
Elia Rütsche <er@itst.ch>
2024-12-11 15:36:27 +0100

    migrated custom script task to existing code file

--- itst/public/js/task.js
+++ itst/public/js/task.js
@@ -4,6 +4,11 @@ frappe.ui.form.on('Task', {
             frm.add_custom_button(__("Offene TS-Positionen"), function() {
                 frappe.set_route("query-report", "Offene TS-Positionen", {"task": frm.doc.name});
             });
+
+            frm.add_custom_button(__('Zugehörige Zammad-Tickets'), function() {
+            	window.open("https://support.itst.ch/#search/" + frm.doc.name, '_blank').focus();
+            }).addClass("btn-primary");
         }
     }
 });
+
