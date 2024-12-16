80c97df9d3ac8c0af35c371c7eff3b7be727e64c
Elia Rütsche <er@itst.ch>
2024-12-16 10:36:28 +0100

    added Doctpye for clockify integration

---
+++ itst/itst/doctype/clockify_import_settings/clockify_import_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, ITST and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Clockify Import Settings', {
+	// refresh: function(frm) {
+
+	// }
+});
