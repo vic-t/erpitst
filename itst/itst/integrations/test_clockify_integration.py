e1905d28dc56661e48e27cd15696f7bacac8af32
Elia Rütsche <er@itst.ch>
2025-01-07 15:43:49 +0100

    dummy test for test cases

---
+++ itst/itst/integrations/test_clockify_integration.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2025, ITST and Contributors
+# See license.txt
+from __future__ import unicode_literals
+
+# import frappe
+import unittest
+
+class TestClockifyIntegration(unittest.TestCase):
+	def test_basic_assertion(self):
+          self.assertTrue(True, "Dies ist nur ein Dummy-Test, der immer True ist.")
