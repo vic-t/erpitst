707765436639a7dae09ae411d808ec5233b254b8
Elia Rütsche <er@itst.ch>
2025-01-08 17:30:13 +0100

    added test_ bevor every method, because frappe needs it

--- itst/itst/tests/test_clockify_integration.py
+++ itst/itst/tests/test_clockify_integration.py
@@ -17,36 +17,36 @@ from itst.itst.integrations.clockify_integration import (
 
 class TestClockifyIntegration(unittest.TestCase):
     
+    def test_basic_assertion(self):
+          self.assertTrue(True, "Dies ist nur ein Dummy-Test, der immer True ist.")
+
     #Tests for parse_duration
-    def should_ReturnCorrectDuration_When_ParsingDurationToDuration(self):
-        duration_str = parse_duration("PT1H30M")
+    def test_should_ReturnCorrectDurationAndHour_When_ParsingDuration(self):
+        hours, duration_str = parse_duration("PT1H30M")
+        self.assertEqual(hours, 1.5)
         self.assertEqual(duration_str, "1:30")
 
-    def should_ReturnCorrectHour_When_ParsingDurationToHour(self):
-        hours = parse_duration("PT2H15M")
-        self.assertEqual(hours, 2.25)
-
-    def should_RetrunError_When_InvalidStringIsPassed(self):
+    def test_should_RetrunError_When_InvalidStringIsPassed(self):
         with self.assertRaises(frappe.ValidationError):
             parse_duration("XYZ")
 
     #Test for parse_hhmm_to_minutes
-    def should_ReturnNumerIn5MinuteInterval_When_ANumberInhhmmFormatIsGiven(self):
+    def test_should_ReturnNumerIn5MinuteInterval_When_ANumberInhhmmFormatIsGiven(self):
         total_minutes = parse_hhmm_to_minutes("2:05")
         self.assertEqual(total_minutes, 125)
 
     #Test for minutes_to_hhmm
-    def should_ReturnNumerinhhmmFormat_When_ANumberIn5MinuteIntervalIsGiven(self):
+    def test_should_ReturnNumerinhhmmFormat_When_ANumberIn5MinuteIntervalIsGiven(self):
         hhmm = minutes_to_hhmm(125)
         self.assertEqual(hhmm, "2:05")
     
     #Test for round_minutes_to_5
-    def should_RoundToTheNext5MinuteIntervalNumber_When_ANumberIsGiven(self):
+    def test_should_RoundToTheNext5MinuteIntervalNumber_When_ANumberIsGiven(self):
         self.assertEqual(round_minutes_to_5(123), 125)
         self.assertEqual(round_minutes_to_5(88), 90)
 
     #Test for convert_iso_to_erpnext_datetime
-    def should_ConvertISOToErpnextDatetime_When_ISOStringIsGiven(self):
+    def test_should_ConvertISOToErpnextDatetime_When_ISOStringIsGiven(self):
         iso_str = "2025-01-01T10:30:00Z" # UTC
         erp_dt = convert_iso_to_erpnext_datetime(iso_str)
 
