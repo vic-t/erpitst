80436fd99966d149abcabee9dd5294647a4d1799
Elia Rütsche <er@itst.ch>
2025-01-08 16:30:30 +0100

    test methods for parse_duration

--- itst/itst/tests/test_clockify_integration.py
+++ itst/itst/tests/test_clockify_integration.py
@@ -3,9 +3,24 @@
 # See license.txt
 from __future__ import unicode_literals
 
-# import frappe
+import frappe
 import unittest
 
+from itst.itst.integrations.clockify_integration import (
+    parse_duration
+)
+
+
 class TestClockifyIntegration(unittest.TestCase):
-	def test_basic_assertion(self):
-          self.assertTrue(True, "Dies ist nur ein Dummy-Test, der immer True ist.")
+    def should_ReturnCorrectDuration_When_ParsingDurationToDuration(self):
+        duration_str = parse_duration("PT1H30M")
+        self.assertEqual(duration_str, "1:30")
+
+    def should_ReturnCorrectHour_When_ParsingDurationToHour(self):
+        hours = parse_duration("PT2H15M")
+        self.assertEqual(hours, 2.25)
+
+    def should_RetrunError_When_InvalidStringIsPassed(self):
+        with self.assertRaises(frappe.ValidationError):
+            parse_duration("XYZ")
+
