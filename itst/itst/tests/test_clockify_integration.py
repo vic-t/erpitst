49934ad496338a4dba9fe15a9cc7fa7430c9998b
Elia Rütsche <er@itst.ch>
2025-01-08 16:37:20 +0100

    Test for parse_hhmm_to_minutes

--- itst/itst/tests/test_clockify_integration.py
+++ itst/itst/tests/test_clockify_integration.py
@@ -7,11 +7,14 @@ import frappe
 import unittest
 
 from itst.itst.integrations.clockify_integration import (
-    parse_duration
+    parse_duration,
+    parse_hhmm_to_minutes
 )
 
 
 class TestClockifyIntegration(unittest.TestCase):
+    
+    #Tests for parse_duration
     def should_ReturnCorrectDuration_When_ParsingDurationToDuration(self):
         duration_str = parse_duration("PT1H30M")
         self.assertEqual(duration_str, "1:30")
@@ -23,4 +26,9 @@ class TestClockifyIntegration(unittest.TestCase):
     def should_RetrunError_When_InvalidStringIsPassed(self):
         with self.assertRaises(frappe.ValidationError):
             parse_duration("XYZ")
+
+    #Tests for parse_hhmm_to_minutes
+    def should_ReturnNumerInThe5Series_When_ANumberInhhmmFormatIsGiven(self):
+        total_minutes = parse_hhmm_to_minutes("2:05")
+        self.assertEqual(total_minutes, 125)
 
