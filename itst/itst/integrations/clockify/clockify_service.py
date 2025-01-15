7a42e9924eb97e0240a58a715b77d12c23a5fb6e
Elia Rütsche <er@itst.ch>
2025-01-15 14:54:33 +0100

    refactored file clockify_service.py

--- itst/itst/integrations/clockify/clockify_service.py
+++ itst/itst/integrations/clockify/clockify_service.py
@@ -1,15 +1,61 @@
 import requests
 import frappe
-from typing import List, Dict
+from typing import List, Dict, Optional
 
 class ClockifyService:
-    def __init__(self, api_key: str, base_url: str):
+    def __init__(self, api_key: str, base_url: str, workspace_id: str):
         self.api_key = api_key
         self.base_url = base_url
+        self.workspace_id = workspace_id
     
-    def fetch_clockify_entries(self, workspace_id: str, user_id: str, week_start_iso: str) -> List[Dict]:
-        endpoint_url = f"{self.base_url}/workspaces/{workspace_id}/user/{user_id}/time-entries"
-        headers = {"X-Api-Key": self.api_key}
+    def _send_request(
+            self, 
+            method: str, 
+            endpoint: str, 
+            headers: Optional[Dict] = None, 
+            params: Optional[Dict] = None, 
+            json_data: Optional[Dict] = None
+        ) -> Dict:
+
+        if headers is None:
+            headers ={}
+        
+        default_headers = {
+            "X-API-Key": self.api_key
+        }
+
+        merged_headers = {**default_headers, **headers}
+
+        api_url = f"{self.base_url}{endpoint}"
+
+        response = response.request(
+            method=method,
+            url=api_url,
+            headers=merged_headers,
+            params=params,
+            json=json_data
+        )
+
+        if response.status_code not in (200, 201):
+            frappe.log_error(
+                title="Clockify API Request Error",
+                message=(
+                    f"Fehler beim Aufrufen der Clockify-API.\n"
+                    f"URL: {api_url}\n"
+                    f"Statuscode: {response.status_code}\n"
+                    f"Response: {response.text}"
+                )
+            )
+            frappe.throw(
+                "Es gab einen Fehler bei der Kommunikation mit der Clockify-API. "
+                "Bitte überprüfen Sie die Error Logs für weitere Details."
+            )
+        
+        return response.json()
+
+
+    def fetch_clockify_entries(self, user_id: str, week_start_iso: str) -> List[Dict]:
+        endpoint_url = f"/workspaces/{self.workspace_id}/user/{user_id}/time-entries"
 
         params = {
             "get-week-before": week_start_iso ,   
@@ -18,21 +64,19 @@ class ClockifyService:
             "page-size": 5000
         }
 
-        response = requests.get(endpoint_url, headers=headers, params=params)
-        if response.status_code != 200:
-            frappe.log_error(
-                f"Fehler beim Abrufen der Einträge: {response.status_code}, {response.text}",
-                "Clockify Fetch Error"
-            )
-            frappe.throw("Fehler beim Abrufen der Einträge von Clockify.")
+        return self._send_request(
+            method="GET",
+            endpoint=endpoint_url,
+            params=params
+        )
 
-        return response.json()
+    def update_clockify_entry(self, entry_id: str, data: dict) -> None:
+        endpoint_url = f"/workspaces/{self.workspace_id}/time-entries/{entry_id}"
+        headers = {"Content-Type": "application/json"}
 
-    def update_clockify_entry(self, workspace_id: str, entry_id: str, data: dict):
-        endpoint_url = f"{self.base_url}/workspaces/{workspace_id}/time-entries/{entry_id}"
-        headers = {"X-Api-Key": self.api_key, "Content-Type": "application/json"}
-
-        response = requests.put(endpoint_url, headers=headers, json=data)
-        if response.status_code != 200:
-            frappe.log_error(f"Fehler beim Aktualisieren des Clockify-Eintrags nach dem Import: Die Aktualisierung des Eintrags mit der ID {entry_id} ist fehlgeschlagen. HTTP-Statuscode: {response.status_code}, Serverantwort: {response.text}. Bitte überprüfen Sie die API-Schlüssel, den Eintrag oder die Verbindung zu Clockify.", "Clockify Update Fehler")
-            frappe.throw(f" Der Clockify-Eintrag mit der ID {entry_id} konnte nach dem Import nicht aktualisiert werden. Dies könnte auf einen API-Fehler oder ein Problem mit der Verbindung zu Clockify hinweisen. Bitte wenden Sie sich an den Administrator, um den Fehler zu beheben.")
+        self._send_request(
+            method="PUT",
+            endpoint=endpoint_url,
+            headers=headers,
+            json_data=data
+        )
