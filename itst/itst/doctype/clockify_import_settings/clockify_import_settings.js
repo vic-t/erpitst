e8cd61444ddd75ac38fa7f960a0cae9620f8963f
Elia Rütsche <er@itst.ch>
2025-02-28 15:26:50 +0100

    adding button for import in doctype

--- itst/itst/doctype/clockify_import_settings/clockify_import_settings.js
+++ itst/itst/doctype/clockify_import_settings/clockify_import_settings.js
@@ -2,7 +2,16 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Clockify Import Settings', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+		if (!frm.is_new()) {
+			frm.add_custom_button(__('Import now'), function() {
+				frappe.call({
+					method: "itst.itst.integrations.clockify_integration.test_connection",
+					callback: function(response) {
+                        console.log("Response from server:", response.message);
+                    }
+				});
+			});
+		}
+	}
 });
