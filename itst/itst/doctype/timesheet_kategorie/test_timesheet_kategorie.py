b586cddfe4f8aba10140c7a503988a2a93f47cc7
Elia Rütsche <er@itst.ch>
2025-04-02 14:00:46 +0200

    edited doctype Kostenstellen

--- itst/itst/doctype/timesheet_kategorie/test_timesheet_kategorie.py
+++ itst/itst/doctype/timesheet_kategorie/test_timesheet_kategorie.py
@@ -6,5 +6,5 @@ from __future__ import unicode_literals
 # import frappe
 import unittest
 
-class TestKostenstellen(unittest.TestCase):
+class TestTimesheet Kategorie(unittest.TestCase):
 	pass
--- itst/itst/doctype/timesheet_kategorie/timesheet_kategorie.js
+++ itst/itst/doctype/timesheet_kategorie/timesheet_kategorie.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2025, ITST and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Kostenstellen', {
+frappe.ui.form.on('Timesheet Kategorie', {
 	// refresh: function(frm) {
 
 	// }
